4535874973af45edbfc76c03f0878e41a5c8b520
Andrey Denisov <andrey.denisov@westwing.de>
2023-09-07 13:34:08 +0200

    chore: add atoms

---
+++ src/ui-kit/atoms/ErrorNotification.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ErrorNotification = styled.div`
+  padding: 8px 14px;
+  border-radius: 8px;
+  background: #ff94b6;
+  font-size: 14px;
+  font-family: 'Inter', sans-serif;
+  font-weight: 600;
+  color: #5030E5;
+  border: none;
+  cursor: pointer;
+`;
