e73d9c31d67fb2f93c1f409e2eb8fd60eca6e60d
Andrey Denisov <andrey.denisov@westwing.de>
2023-09-07 23:51:14 +0200

    style: fix eslint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { router } from './router';
-import {Provider} from "react-redux";
-import store from "./redux";
+import store from './redux';
 
 function App() {
   return (
